388e05420a3be970966991e0d6d2ae5d44ae8750
yukke271 <yukke271@gmail.com>
2024-01-05 00:07:29 +0900

    update import module @vee-validate/nuxt

--- nuxt/app/VRCFavoLister/nuxt.config.ts
+++ nuxt/app/VRCFavoLister/nuxt.config.ts
@@ -9,4 +9,15 @@ export default defineNuxtConfig({
     },
   },
   modules: ["@vee-validate/nuxt"],
+  veeValidate: {
+    // disable or enable auto imports
+    autoImports: true,
+    // Use different names for components
+    componentNames: {
+      Form: "VeeForm",
+      Field: "VeeField",
+      FieldArray: "VeeFieldArray",
+      ErrorMessage: "VeeErrorMessage",
+    },
+  },
 });
