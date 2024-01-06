ce0adba4f6090b790b66a357f6a093f580f9f4e8
yukke271 <yukke271@gmail.com>
2024-01-07 04:01:40 +0900

    create components

---
+++ nuxt/app/VRCFavoLister/components/parser/world/ParserWorldCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <h3>{{ props.world.name }}</h3>
+    </v-card-title>
+    <v-card-text>
+      <a
+        :href="`https://vrchat.com/home/world/${props.world.id}`"
+        target="_blank"
+        >{{ props.world.name }}</a
+      >
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  world: World;
+}>();
+</script>
