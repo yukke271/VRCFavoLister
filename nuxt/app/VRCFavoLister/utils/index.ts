d26a66390a4be6223df82078512d840a9172a6b1
yukke271 <yukke271@gmail.com>
2024-01-07 10:28:38 +0900

    update utils,add device check func

--- nuxt/app/VRCFavoLister/utils/index.ts
+++ nuxt/app/VRCFavoLister/utils/index.ts
@@ -1,3 +1,14 @@
 // 配下のディレクトリのオートインポートを有効にする
 export { devLog } from "@/utils/logging/devLog";
 export { worldsParser } from "@/utils/parser/worldsParser";
+export const isMobile = () => {
+  // サイトにアクセスしている端末がモバイル端末かどうかを画面の幅で判定する
+  if (
+    typeof window.matchMedia === "function" &&
+    window.matchMedia("(max-device-width: 640px)").matches
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
