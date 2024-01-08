be7900a1eb19cf15f91e16ac329e0c9bfc461d3e
yukke271 <yukke271@gmail.com>
2024-01-08 09:28:35 +0900

    update conponent,style

--- nuxt/app/VRCFavoLister/components/parser/world/ParserWorldCard.vue
+++ nuxt/app/VRCFavoLister/components/parser/world/ParserWorldCard.vue
@@ -1,20 +1,105 @@
 <template>
-  <v-card>
-    <v-card-title>
-      <h3>{{ props.world.name }}</h3>
-    </v-card-title>
-    <v-card-text>
-      <a
-        :href="`https://vrchat.com/home/world/${props.world.id}`"
-        target="_blank"
-        >{{ props.world.name }}</a
-      >
-    </v-card-text>
-  </v-card>
+  <a
+    :href="`https://vrchat.com/home/world/${props.world.id}`"
+    target="_blank"
+    class="container"
+  >
+    <v-card :image="props.world.thumbnailImageUrl" height="200" class="card">
+      <v-card-text class="incard">
+        <p class="title">{{ props.world.name }}</p>
+      </v-card-text>
+    </v-card>
+    <div :class="`pc ${isPC ? '' : 'grayout'}`">
+      <span>PC</span>
+    </div>
+    <div :class="`quest ${isQuest ? '' : 'grayout'}`">
+      <span>Quest</span>
+    </div>
+  </a>
 </template>
 
 <script setup lang="ts">
 const props = defineProps<{
   world: World;
 }>();
+
+let isPC = false;
+let isQuest = false;
+
+props.world.unityPackages.forEach((unityPackage) => {
+  if (unityPackage.platform === "standalonewindows") {
+    isPC = true;
+  }
+  if (unityPackage.platform === "android") {
+    isQuest = true;
+  }
+});
 </script>
+
+<style scoped>
+.container {
+  position: relative;
+}
+.incard {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  color: white;
+  padding: 0.5rem;
+  box-sizing: border-box;
+}
+.title {
+  font-size: 1rem;
+  font-weight: bold;
+}
+.pc {
+  position: absolute;
+  display: flex;
+
+  top: 5%;
+  right: 15%;
+  width: 2rem;
+  height: 2rem;
+
+  justify-content: center;
+  align-items: center;
+
+  border: 2px solid white;
+  border-radius: 50%;
+
+  font-size: 0.6rem;
+  color: black;
+
+  background-color: rgba(0, 150, 250, 0.8);
+
+  z-index: 1;
+}
+.quest {
+  position: absolute;
+  display: flex;
+
+  top: 5%;
+  right: 5%;
+  width: 2rem;
+  height: 2rem;
+
+  justify-content: center;
+  align-items: center;
+
+  border: 2px solid white;
+  border-radius: 50%;
+
+  font-size: 0.6rem;
+  color: black;
+
+  background-color: rgb(0, 250, 50, 0.8);
+
+  z-index: 1;
+}
+.grayout {
+  background-color: rgb(100, 100, 100, 0.6);
+  color: white;
+  z-index: 0;
+}
+</style>
